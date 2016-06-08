9d96d2109cbf682e9d2a9a7a0cb1473d8dd23770
Lukas Udstuen <lukasudstuen@MacBook-Pro-de-Lukas.local>
2016-06-08 10:50:20 -0500

    removing public folder

--- server/app.js
+++ server/app.js
@@ -1,10 +1,8 @@
+
 var http = require('http');
 
-
-http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.write('Hello, world!');
-  console.log('Hello, world!');
-  res.end();
+http.createServer(function(req, res){
+ res.writeHead(200);
+ res.write('Hello World!');
+ res.end();
 }).listen(process.env.PORT || 3000);
-console.log("Server is listening");
