828bf78f88c8cc6a81ca73669988344cb1532f80
Lukas Udstuen <lukasudstuen@MacBook-Pro-de-Lukas.local>
2016-06-08 11:06:22 -0500

    removing res

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@
 var http = require('http');
 
 http.createServer(function(req, res){
- res.writeHead(200);
- res.write('Hello World!');
- res.end();
-}).listen(process.env.PORT);
+ // res.writeHead(200);
+ console.log("Made it to create server");
+ // res.write('Hello World!');
+ // res.end();
+}).listen(process.env.PORT || 3000);
